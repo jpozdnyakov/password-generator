3ae345614c12a3dea95451c2c7a968ac4ba60493
jpozdnyakov <juliedubovaya@hotmail.com>
2022-01-16 15:07:02 -0800

    function parameters

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,7 +6,32 @@ var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
 var loweCase = ['a', 'b', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var specialChar = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '=', '+', ':', '{', '}', '?', '/'];
 
+//Function to determine parameters of password
+function questions() {
+  var isValid = false;
+  do {
+    var length = prompt("Choose password lenth between 8 and 128 characters");
+    var askNumbers = confirm("Do you want your password to include numbers?");
+    var askLowerCase = confirm("Do you want your password to include lower case letters?");
+    var askUpperCase = confirm("Do you want your password to include upper case letters?");
+    var askSpecialChar = confirm("Do you want your password to include special characters?");
+    var responses = {
+      length: length,
+      askNumbers: askNumbers,
+      askLowerCase: askLowerCase,
+      askUpperCase: askUpperCase,
+      askSpecialChar: askSpecialChar
+    }
+    if ((length < 8)||(length > 128))
+    alert("Choose a number between 8 and 128");
+    else if ((!askNumbers)&&(!askLowerCase)&&(!askUpperCase)&&(!askSpecialChar))
+    alert("Must choose at least one type.");
+    else
+    isValid = true;
 
+  } while(!isValid);
+  return responses;
+}
 
 // Write password to the #password input
 function writePassword() {
